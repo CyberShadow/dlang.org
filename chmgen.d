58b4e634bc46f9e844add70ce242664ce3a9f9bf
Vladimir Panteleev <vladimir@thecybershadow.net>
2015-01-28 04:43:24 +0000

    WIP

--- chmgen.d
+++ chmgen.d
@@ -3,13 +3,13 @@
 // Placed in the Public Domain
 // Written in the D Programming Language, version 2
 
-//import std.algorithm : min, canFind, sort;
+import std.algorithm;
 //import std.array;
 //import std.ascii;
 import std.exception;
 import std.file;
 import std.range;
-import std.stdio : stderr;
+import std.stdio : File, stderr;
 import std.string;
 import std.regex;
 import std.path;
@@ -19,12 +19,13 @@ import std.path;
 enum ROOT = `.`;
 
 // ********************************************************************
-/*
-string backSlash(string s)
+
+string fixSlashes(string s)
 {
-	return s.replace(`/`, `\`);
+	return s.replace(`\`, `/`);
 }
 
+/*
 bool contains(string s, string sub) { return s.indexOf(sub) >= 0; }
 
 RegexMatch!string match;
@@ -41,45 +42,57 @@ string getAnchor(string s)
 	return i<0 ? "" : s[i..$];
 }
 
-string removeAnchor(string s)
+string stripAnchor(string s)
 {
 	int i = s.indexOf('#');
 	return i<0 ? s : s[0..i];
 }
+*/
+
+string getAnchor(string s)
+{
+	return s.findSplitBefore("#")[1];
+}
+
+string stripAnchor(string s)
+{
+	return s.findSplit("#")[0];
+}
 
 string absoluteUrl(string base, string url)
 {
-	if (url.contains("://"))
+	if (url.canFind("://"))
 		return url;
 
-	base = base.backSlash();
-	url  = url.backSlash();
+	base = base.fixSlashes();
+	url  = url.fixSlashes();
 	enforce(url.length, "Empty URL");
 
 	if (url[0]=='#')
 		return base ~ url;
 
-	auto baseParts = base.split(`\`);
-	baseParts = baseParts[0..$-1];
+	auto pathSegments = base.split(`/`)[0..$-1];
+	auto urlSegments = url.split(`/`);
 
-	while (url.startsWith(`..\`))
+	while (urlSegments.startsWith([`..`]))
 	{
-		url = url[3..$];
-		baseParts = baseParts[0..$-1];
+		urlSegments = urlSegments[1..$];
+		pathSegments = pathSegments[0..$-1];
 	}
-	return baseParts.join(`\`) ~ `\` ~ url;
+	return (pathSegments ~ urlSegments).join(`/`);
 }
 
+/*
 string adjustPath(string s)
 {
-	if (s.startsWith(ROOT ~ `\`))
+	if (s.startsWith(ROOT ~ `/`))
 		s = "chm" ~ s[ROOT.length..$];
 	return s;
 }
 */
 string adjustPath(string s)
 {
-	enforce(s.startsWith(ROOT ~ `\`));
+	enforce(s.startsWith(ROOT ~ `/`), "Bad path: " ~ s);
 	return "chm" ~ s[ROOT.length..$];
 }
 /*
@@ -119,16 +132,12 @@ class Nav
 Nav nav;
 class Page
 {
-	string newFileName;
-	string title;
-	string src;
-//	bool[string] anchors;
+	string newFileName, title, src;
 }
-/*
+
 struct KeyLink
 {
-	string anchor;
-	string title;
+	string anchor, title;
 
 	this(string anchor, string title)
 	{
@@ -136,19 +145,18 @@ struct KeyLink
 		this.title  = title.strip();
 	}
 }
-*/
+
 // ********************************************************************
+
 Page[string] pages;
-/*
 KeyLink[string][string] keywords;   // keywords[keyword][original url w/o anchor] = anchor/title
 string[string] keyTable;
-*/
-/*
+
 void addKeyword(string keyword, string link, string title = null)
 {
 	keyword = keyword.strip();
-	string file = link.removeAnchor();
-	file = file.backSlash();
+	string file = link.stripAnchor();
+	file = file.fixSlashes();
 	string anchor = link.getAnchor();
 
 	if (!title && keyword in keywords && file in keywords[keyword])   // when title is present, it overrides any existing anchors/etc.
@@ -167,7 +175,6 @@ void addKeyword(string keyword, string link, string title = null)
 	else
 		keyTable[keyword] = keyword;
 }
-*/
 
 Regex!char re(string pattern, alias flags = [])()
 {
@@ -179,20 +186,17 @@ Regex!char re(string pattern, alias flags = [])()
 
 void main()
 {
-	// clean up
-	if (exists("chm"))
-		rmdirRecurse("chm");
-	mkdir("chm");
+	mkdirRecurse("chm");
 
-	enforce(exists(ROOT ~ `\phobos\index.html`),
+	enforce(exists(ROOT ~ `/phobos/index.html`),
 		"Phobos documentation not present. Please place Phobos documentation HTML files into the \"phobos\" subdirectory.");
 
 	string[] files = chain(
-		dirEntries(ROOT            , "*.html", SpanMode.shallow),
-		dirEntries(ROOT ~ "/phobos", "*.html", SpanMode.shallow),
-		dirEntries(ROOT ~ "/js"              , SpanMode.shallow),
-		dirEntries(ROOT ~ "/css"             , SpanMode.shallow),
-		dirEntries(ROOT ~ "/images"          , SpanMode.shallow),
+		dirEntries(ROOT ~ "/"       , "*.html", SpanMode.shallow),
+		dirEntries(ROOT ~ "/phobos/", "*.html", SpanMode.shallow),
+	//	dirEntries(ROOT ~ "/js/"              , SpanMode.shallow),
+		dirEntries(ROOT ~ "/css/"             , SpanMode.shallow),
+		dirEntries(ROOT ~ "/images/", "*.*"   , SpanMode.shallow),
 		only(ROOT ~ "/favicon.ico")
 	).array();
 
@@ -204,9 +208,7 @@ void main()
 	auto re_heading_link = regex(`<h2><a href="([^"]*)"( title="([^"]*)")?>(.*)</a></h2>`);
 	auto re_nav_link     = regex(`<li><a href="([^"]*)"( title="(.*)")?>(.*)</a>`);
 	auto re_anchor_1     = regex(`<a name="\.?([^"]*)">(<\w{1,2}>)*([^<]+)<`);
-	auto re_anchor_2     = regex(`<a name=([^">]*)>(<\w{1,2}>)*([^<]+)<`);
 	auto re_anchor_1h    = regex(`<a name="\.?([^"]*)"`);
-	auto re_anchor_2h    = regex(`<a name=([^">]*)>`);
 	auto re_link         = regex(`<a href="([^"]*)">(<\w{1,2}>)*([^<]+)<`);
 	auto re_link_pl      = regex(`<li><a href="(http://www.digitalmars.com/d)?/?(\d\.\d)?/index.html" title="D Programming Language \d\.\d">`);
 	auto re_def          = regex(`<dt><big>(.*)<u>([^<]+)<`);
@@ -229,26 +231,27 @@ void main()
 				stderr.writeln("Processing ", fileName);
 				src = fileName.readText();
 				string[] lines = src.splitLines();
-				string[] newlines = null;
+//				string[] newlines = null;
 //				bool skip, innavblock, intoctop;
 //				int dl = 0;
 //				anchors[""] = true;
 
 				Nav[] navStack = [nav];
-				if (fileName.startsWith(ROOT ~ `\phobos\`))
+				if (fileName.startsWith(ROOT ~ `/phobos/`))
 				{
 					navStack ~= navStack[$-1].findOrAdd("Documentation", null);
-					navStack ~= navStack[$-1].findOrAdd("Library Reference", `chm\phobos\index.html`);
+					navStack ~= navStack[$-1].findOrAdd("Library Reference", `chm/phobos/index.html`);
 					navStack ~= navStack[$-1].findOrAdd(null, null);
 				}
 				else
 					navStack ~= null;
-				bool foundNav = false;
 
-				foreach (origLine; lines)
+				bool foundBody, redirect;
+
+				foreach (ref line; lines)
 				{
-					scope(failure) stderr.writeln("Error while processing line: ", origLine);
-					string line = origLine;
+					scope(failure) stderr.writeln("Error while processing line: ", line);
+				//	string line = origLine;
 				//	bool nextSkip = skip;
 
 				//	if (line.test(re_link_pl))
@@ -267,8 +270,12 @@ void main()
 				//	if (line.test(re_title3))
 				//		if (title=="")
 				//			title = strip(/*re_title2*/match.captures[1]);
+
 					RegexMatch!string m;
-					if (!!(m = line.match(re!`^<title>.* - D Programming Language</title>$`)))
+
+					// Find title
+
+					if (!!(m = line.match(re!`^<title>(.*) - D Programming Language</title>$`)))
 						title = m.captures[1];
 
 				//	if (line.test(re_anchor_1h))
@@ -346,26 +353,35 @@ void main()
 						dl--;
 				+/
 
-					if (line.test(re_anchor_1))
-						addKeyword(/*re_anchor*/match.captures[3], fileName ~ "#" ~ /*re_anchor*/match.captures[1]);
-					else
-					if (line.test(re_anchor_2))
-						addKeyword(/*re_anchor_2*/match.captures[3], fileName ~ "#" ~ /*re_anchor_2*/match.captures[1]);
-					else
-					if (line.test(re_anchor_1h))
-						addKeyword(/*re_anchor*/match.captures[1], fileName ~ "#" ~ /*re_anchor*/match.captures[1]);
-					else
-					if (line.test(re_anchor_2h))
-						addKeyword(/*re_anchor_2*/match.captures[1], fileName ~ "#" ~ /*re_anchor_2*/match.captures[1]);
+					// Add document CSS class
 
-					if (line.test(re_link))
-						if (!/*re_link*/match.captures[1].startsWith("http://"))
-							addKeyword(/*re_link*/match.captures[3], absoluteUrl(fileName, /*re_link*/match.captures[1]));
+					if (!!(m = line.match(re!`^(<body id='.*' class='.*)('>)$`)))
+					{
+						line = m.captures[1] ~ " chm" ~ m.captures[2];
+						foundBody = true;
+					}
 
-					// skip "digg this"
-					if (line.contains(`<script src="http://digg.com/tools/diggthis.js"`))
-						skip = true;
+					if (line.match(re!`^<meta http-equiv="Refresh" content="0; URL=`))
+						redirect = true;
 
+					// Find anchors
+
+					if (!!(m = line.match(re!`<a name="(\.?[^"]*)">(<\w{1,2}>)*([^<]+)<`)))
+						addKeyword(m.captures[3], fileName ~ "#" ~ m.captures[1]);
+					else
+					if (!!(m = line.match(re!`<a name="(\.?([^"]*))">`)))
+						addKeyword(m.captures[2], fileName ~ "#" ~ m.captures[1]);
+
+					if (!!(m = line.match(re!`<a href="([^"]*)">(<\w{1,2}>)*([^<]+)<`)))
+						if (!m.captures[1].canFind("://"))
+							addKeyword(m.captures[3], absoluteUrl(fileName, m.captures[1]));
+
+					// Disable scripts
+
+					if (line.canFind(`<script`))
+						line = null;
+
+				/+
 					while (!skip && line.test(re_extern_js))
 					{
 						auto url = match.captures[1].replace(`\`, `/`);
@@ -387,7 +403,6 @@ void main()
 
 					while (line.test(re_res_path))
 						line = line.replace(match.captures[0], `<` ~ match.captures[1] ~ ` src="` ~ "../".replicate(fileName[ROOT.length+1..$].split(dirSeparator).length-1) ~ match.captures[2]);
-
 					// skip Google ads
 					if (line.startsWith(`<!-- Google ad -->`))
 						skip = nextSkip = true;
@@ -410,14 +425,19 @@ void main()
 					// Work around JS bug in run.js
 					if (line.contains(`<head`))
 						newlines ~= `<script>mainPage = [];</script>`;
+				+/
 				}
 
-				if (!foundNav)
-					writeln("Warning: Page not found in navigation");
+				enforce(foundBody || redirect, "Body not found");
 
-				src = join(newlines, std.ascii.newline[]);
+			//	if (!foundNav)
+			//		stderr.writeln("Warning: Page not found in navigation");
+
+			//	src = join(newlines, std.ascii.newline[]);
+				src = lines.join("\r\n");
 				std.file.write(newFileName, src);
 			}
+		/*
 			else
 			if (fileName.endsWith(`.css`))
 			{
@@ -434,9 +454,10 @@ void main()
 				src = join(newlines, std.ascii.newline[]);
 				std.file.write(newFileName, src);
 			}
+		*/
 			else
 			{
-				writeln("Copying "~fileName);
+				stderr.writeln("Copying ", fileName);
 				copy(fileName, newFileName);
 			}
 		}
@@ -459,7 +480,7 @@ Compatibility=1.1 or later
 Compiled file=d.chm
 Contents file=d.hhc
 Default Window=main
-Default topic=chm\index.html
+Default topic=chm/index.html
 Display compile progress=No
 Full-text search=Yes
 Index file=d.hhk
@@ -467,14 +488,14 @@ Language=0x409 English (United States)
 Title=D
 
 [WINDOWS]
-main="D Programming Language","d.hhc","d.hhk","chm\index.html","chm\index.html",,,,,0x63520,,0x380e,[0,0,800,570],0x918f0000,,,,,,0
+main="D Programming Language","d.hhc","d.hhk","chm/index.html","chm/index.html",,,,,0x63520,,0x380e,[0,0,800,570],0x918f0000,,,,,,0
 
 [FILES]`);
 	string[] htmlList;
 	foreach (page;pages)
 		if (page.newFileName.endsWith(`.html`))
 			htmlList ~= page.newFileName;
-	htmlList.sort;
+	htmlList.sort();
 	foreach (s; htmlList)
 		f.writeln(s);
 	f.writeln(`
@@ -519,22 +540,22 @@ main="D Programming Language","d.hhc","d.hhk","chm\index.html","chm\index.html",
 	// ************************************************************
 
 	string[] keywordList;
-	foreach (keyNorm,urlList;keywords)
+	foreach (keyNorm, urlList; keywords)
 		keywordList ~= keyNorm;
 	//keywordList.sort;
-	sort!q{icmp(a, b) < 0}(keywordList);
+	keywordList.sort!q{icmp(a, b) < 0};
 
 	f.open("d.hhk", "wt");
 	f.writeln(
 `<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN"><HTML><BODY>
 <UL>`);
-	foreach (keyNorm;keywordList)
+	foreach (keyNorm; keywordList)
 	{
 		auto urlList = keywords[keyNorm];
 		f.writeln(
 `	<LI> <OBJECT type="text/sitemap">
 		<param name="Name" value="`, keyTable[keyNorm], `">`);
-		foreach (url,link;urlList)
+		foreach (url, link; urlList)
 			if (url in pages)
 			{
 				f.writeln(
